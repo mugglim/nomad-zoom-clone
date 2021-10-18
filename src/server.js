ce63cb9bd7ffba0c74f15b703d18150d3399e737
Jaeuk Lim <applewodnr@naver.com>
2021-10-18 23:16:37 +0900

    comment: server.js에 주석 추가

--- src/server.js
+++ src/server.js
@@ -13,25 +13,27 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
-// socket
 wsServer.on('connection', socket => {
+	// Get Stream -> new RTCPeerConnection -> Create Offer
 	socket.on('join_room', roomName => {
 		socket.join(roomName);
 		socket.to(roomName).emit('welcome');
 	});
 
+	// PeerA -> PeerB
 	socket.on('offer', (offer, roomName) => {
 		socket.to(roomName).emit('offer', offer);
 	});
 
+	// PeerA <- PeerB
 	socket.on('answer', (answer, roomName) => {
 		socket.to(roomName).emit('answer', answer);
 	});
 
+	// PeerA <-> PeerB
 	socket.on('ice', (ice, roomName) => {
 		socket.to(roomName).emit('ice', ice);
 	});
 });
 
-// http
 httpServer.listen(3000, () => console.log('Listening on http://localhost:3000'));
