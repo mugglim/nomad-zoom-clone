d550c8a434363a95b16afc597ca04dd4615a7a05
Jaeuk Lim <applewodnr@naver.com>
2021-10-18 15:59:28 +0900

    chore: view, static 미들웨어 설정

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
 
-console.log('Hello!');
-app.listen(3000);
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(3000, () => console.log('listening on 3000 port'));
